e492f2869cb8ebdad592a81b452db95fca520860
MICAELA RICCARDI <47574710@ort>
2023-06-15 08:59:49 -0300

    controllers hechos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,52 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag listaPartidos = BD.ListarPartidos();
+        return View();
+    }
+
+    public IActionResult VerDetallePartido(int idPartido)
+    {
+        ViewBag partido = BD.VerInfoPartido(idPartido);
+        ViewBag listaCandidatos = BD.ListarCandidatos(idPartido);
+        return View();
+    }
+
+    public IActionResult VerDetalleCandidato(int idCandidato)
+    {
+        ViewBag candidato = BD.VerInfoCandidato(idCandidato);
+        return View();
+    }
+
+     public IActionResult AgregarCandidato(int idPartido)
+    {
+        ViewBag IdPartido = idPartido;
+        return View();
+    }
+
+    public IActionResult GuardarCandidato(Candidato can)
+    {
+        BD.AgregarCandidato(can);
+        ViewBag partido = BD.VerInfoPartido(idPartido);
+        ViewBag listaCandidatos = BD.ListarCandidatos(idPartido);
+        return View(VerDetallePartido);
+    }
+
+    public IActionResult EliminarCandidato(int idCandidato, int idPartido)
+    {
+        BD.EliminarCandidato(idCandidato);
+        ViewBag partido = BD.VerInfoPartido(idPartido);
+        ViewBag listaCandidatos = BD.ListarCandidatos(idPartido);
+        return View(VerDetallePartido);
+    }
+
+    public IActionResult Elecciones() 
+    {
+        return View(infoElecciones);
+    }
+    /* AGREGAR ESTAS VIEWS, NO ESTÁN EN LA CONSIGNA */
+    public IActionResult Creditos() 
+    {
         return View();
     }
 
